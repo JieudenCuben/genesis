c3177c6cfe243454694db042b7c6f769a1228c81
JieudenCuben <isaacjieu98@gmail.com>
2024-10-17 15:16:57 +0800

    Initial commit

---
+++ photo_splitter.py
@@ -0,0 +1,20 @@
+import dlib
+import cv2
+import matplotlib.pyplot as plt
+
+image_path = '/Users/isaacjieu/Documents/projects/genesis/test_images/x-axis.jpeg'
+image = cv2.imread(image_path)
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+detector = dlib.get_frontal_face_detector()
+
+faces = detector(gray)
+
+for face in faces:
+    x, y, w, h = (face.left(), face.top(), face.width(), face.height())
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+plt.axis('off')
+plt.show()
